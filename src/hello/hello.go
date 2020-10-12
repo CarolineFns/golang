62c2910000603b25baadb4e2b2dee7b57063249a
CarolineFns <carolineadm.santos@gmail.com>
2020-10-12 10:19:27 -0300

    Configuração pacotes necessários para programar em go, e inicio do programa de monitoramento

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -8,19 +8,15 @@ func main() {
 	nome := "Carol"
 	versao := 1.1
 
-	fmt.Println("Bem vindo ao programa, ", nome)
-	fmt.Println("Este programa está na versão: ", versao)
+	fmt.Println("Olá,", nome, "!")
+	fmt.Println("Bem vindo ao programa de monitoramento")
+	fmt.Println("Este programa está na versão:", versao)
 
-	//fmt.Println("O tipo da variavel é: ", reflect.TypeOf(nome))
 	fmt.Println("1 - Iniciar monitoramento")
 	fmt.Println("2 - Exibir logs")
 	fmt.Println("0 - Sair do programa")
 
 	var comando int
-	fmt.Scanf("%d", &comando)
 	fmt.Scan(&comando)
-
-	fmt.Println("Endereço na memória da var comando: ", &comando)
-	fmt.Println("O comando escolhido foi: ", comando)
 
 }

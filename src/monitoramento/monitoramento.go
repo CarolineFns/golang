5f22d9f185ee5dbad40118056ed32ab93f139ee2
CarolineFns <carolineadm.santos@gmail.com>
2020-10-12 23:23:37 -0300

    implementado método para testar a cada tempo solicitado os sites do slice (arry)

--- src/monitoramento/monitoramento.go
+++ src/monitoramento/monitoramento.go
@@ -4,25 +4,33 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 )
+
+const monitoramento = 2
+const delay = 5
 
 func main() {
 	exibeIntroducao()
-	exibeMenu()
 
-	comando := leComando()
+	for {
+		exibeMenu()
+		comando := leComando()
 
-	switch comando {
-	case 1:
-		iniciarMonitoramento()
-	case 2:
-		fmt.Println("Exibindo logs...")
-	case 0:
-		fmt.Println("Saindo do programa...")
-		os.Exit(0)
-	default:
-		fmt.Println("Comando não reconhecido.")
-		os.Exit(-1)
+		switch comando {
+		case 1:
+			iniciarMonitoramento()
+		case 2:
+			fmt.Println("Exibindo logs...")
+		case 0:
+			fmt.Println("Saindo do programa...")
+			os.Exit(0)
+		default:
+			fmt.Println("Comando não reconhecido.")
+			os.Exit(-1)
+		}
+
+		fmt.Println("")
 	}
 }
 
@@ -53,7 +61,32 @@ func leComando() int {
 
 func iniciarMonitoramento() {
 	fmt.Println("Monitoramento...")
-	site := "https://alura.com.br"
+
+	sites := []string{
+		"https://random-status-code.herokuapp.com/",
+		"https://alura.com.br/",
+		"https://google.com.br/",
+		"http://facebook.com.br/"}
+
+	for i := 0; i < monitoramento; i++ {
+		for i, site := range sites {
+			fmt.Println("Testando site", i, ":", site)
+			testaSite(site)
+		}
+
+		time.Sleep(delay * time.Second)
+		fmt.Println("")
+	}
+
+	fmt.Println("")
+}
+
+func testaSite(site string) {
 	resp, _ := http.Get(site)
-	fmt.Println(resp)
+
+	if resp.StatusCode == 200 {
+		fmt.Println("Site:", site, "foi carregado com sucesso!")
+	} else {
+		fmt.Println("Site:", site, "está com problemas. Status Code:", resp.StatusCode)
+	}
 }
